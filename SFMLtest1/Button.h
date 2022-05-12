e3908473fa4c9ea5c25ad896509712023ac065f9
Guido Asbun <guido@asbun.io>
2022-05-12 09:49:41 -0700

    created button class

---
+++ SFMLtest1/Button.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+class Button
+{
+private:
+
+public:
+	Button();
+	virtual ~Button();
+
+
+};
+
